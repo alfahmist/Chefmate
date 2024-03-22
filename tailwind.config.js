651aafa19a6e221d6c5d411ed923da2aa6f0ca8d
Alfahmist <gst.alfahmi@gmail.com>
2024-03-22 17:59:38 +0700

    add backgroundImage for article

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#f97316",
-      },
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "article-1": "url('./img/makanan-artikel.jpg')",
+                "article-2": "url('./img/roasted-chicken.jpg')",
+                "article-3": "url('./img/ayam-goreng.jpg')",
+                "article-4": "url('./img/mie-goreng-telur.jpg')",
+            },
+            colors: {
+                primary: "#f97316",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
