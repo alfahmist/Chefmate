9a65ca94a0e1b6a92d2d09ac45feec6940a25619
alfahmist <gst.alfahmi@gmail.com>
2024-03-25 19:26:26 +0700

    Add autoplay slider

--- src/slider.js
+++ src/slider.js
@@ -1,22 +1,26 @@
 // Initialize Swiper
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 5,
-    centeredSlides: true,
-    pagination: {
-        el: ".swiper-pagination",
-        bulletClass: "swiper-pagination-bullet",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        // when window width is >= 640px
-        640: {
-            slidesPerView: 6,
-            spaceBetween: 10,
-        },
-    },
+	slidesPerView: 2,
+	spaceBetween: 5,
+	centeredSlides: true,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false,
+	},
+	pagination: {
+		el: ".swiper-pagination",
+		bulletClass: "swiper-pagination-bullet",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	breakpoints: {
+		// when window width is >= 640px
+		640: {
+			slidesPerView: 6,
+			spaceBetween: 10,
+		},
+	},
 });
