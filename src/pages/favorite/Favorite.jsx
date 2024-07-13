12ac3458478f91031e8d3f083aa564f67095ede2
SadamMahendra <sadam.mahendra@gmail.com>
2024-07-13 23:22:52 +0700

    menambahkan fungsi delete di favorite

--- src/pages/favorite/Favorite.jsx
+++ src/pages/favorite/Favorite.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { Link } from "react-router-dom";
-// import img from "../../assets/img/ayam.jpg";
 import MainLayout from "../../layout/MainLayout";
 
 import { useEffect, useState } from "react";
@@ -37,6 +36,22 @@ function index() {
 
     fetchData();
   }, [token]);
+
+  const deleteFavorite = async (id) => {
+    try {
+      const response = await api.delete(`/favorite/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      alert(response.data.message);
+
+      setData(data.filter((item) => item.id !== id));
+    } catch (error) {
+      console.error("Error deleting favorite:", error);
+      alert("Error deleting favorite");
+    }
+  };
 
   if (!data) {
     return (
@@ -122,51 +137,41 @@ function index() {
             {data.map((item, index) => {
               return (
                 <div className="relative w-[240px]" key={index}>
-                  <button className="w-10 h-10 bg-orange-500 rounded-full flex absolute right-4 top-4 text-slate-50 cursor-pointer duration-500 hover:bg-orange-700 hover:text-slate-200 z-10">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="w-7 h-7 inline-block m-auto"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
-                      />
-                    </svg>
+                  <button
+                    className="w-10 h-10 bg-orange-500 rounded-full flex absolute right-4 top-4 text-slate-50 cursor-pointer duration-500 hover:bg-orange-700 hover:text-slate-200 z-10 items-center justify-center"
+                    onClick={() => deleteFavorite(item.id)}
+                  >
+                    X
                   </button>
-                  <Link to={`/detail/${item.id}`}>
-                    <div className="w-full overflow-hidden rounded-2xl h-64">
+                  <div className="w-full overflow-hidden rounded-2xl h-64">
+                    <Link to={`/detail/${item.id_recipe}`}>
                       <img
                         src={`http://localhost:3000${item.foto_recipe_url}`}
                         alt={data.judul}
                         className="cursor-pointer hover:scale-105 duration-500 h-full w-full object-cover object-center "
                       />
-                    </div>
-                    <div className="flex flex-row justify-start gap-x-2 my-4">
-                      <button className="border-2 border-orange-500 text-orange-500 rounded-xl flex p-1 hover:text-white hover:bg-orange-500 duration-500 cursor-pointer">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 24 24"
-                          fill="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-                        <span className="font-bold">{item.durasi}</span>
-                      </button>
-                    </div>
-                    <p className="text-slate-700 text-xl font-bold hover:text-green-600 duration-500 cursor-pointer truncate">
-                      {item.judul}
-                    </p>
-                  </Link>
+                    </Link>
+                  </div>
+                  <div className="flex flex-row justify-start gap-x-2 my-4">
+                    <button className="border-2 border-orange-500 text-orange-500 rounded-xl flex p-1 hover:text-white hover:bg-orange-500 duration-500 cursor-pointer">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      <span className="font-bold">{item.durasi}</span>
+                    </button>
+                  </div>
+                  <p className="text-slate-700 text-xl font-bold hover:text-green-600 duration-500 cursor-pointer truncate">
+                    {item.judul}
+                  </p>
                 </div>
               );
             })}
